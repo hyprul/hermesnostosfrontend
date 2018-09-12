c7b4445d989926ed0a935eab0f69b6cff41e2bd4
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-09-12 10:07:08 -0500

    support create, update, and delete for Traveler

--- travelApp_backend/app/controllers/travelers_controller.rb
+++ travelApp_backend/app/controllers/travelers_controller.rb
@@ -10,8 +10,38 @@ class TravelersController < ApplicationController
     render json: traveler
   end
 
+  def create
+    traveler = Traveler.new(traveler_params);
+    if traveler.save
+      render json: traveler
+    else
+      render json: traveler.errors
+    end
+  end
+
+  def update
+    traveler = Traveler.find(params[:id])
+    if traveler.update(traveler_params)
+      render json: traveler
+    else
+      render json: traveler.errors
+    end
+  end
+
+  def destroy
+    traveler = Traveler.find(params[:id])
+    if traveler.destroy
+      render json: traveler
+    else
+      render json: traveler.errors
+    end
+  end
+
 
   private
 
+  def traveler_params
+    params.require(:traveler).permit(:name, :age, :picture_url);
+  end
 
 end
